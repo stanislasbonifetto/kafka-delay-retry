e74db956206f0e4b1910af8d382a43ee1617c699
Stanislas Bonifetto <stanislas.bonifetto@gmail.com>
2020-05-15 16:44:00 +0100

    create a rudimantal consumer and producer

--- src/main/java/it/stanislas/kafka/delay/ProducerA.java
+++ src/main/java/it/stanislas/kafka/delay/ProducerA.java
@@ -1,7 +1,60 @@
 package it.stanislas.kafka.delay;
 
+import io.reactivex.rxjava3.core.Observable;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 /***
  * produce a message A in topic-a
  */
 public class ProducerA {
+
+    private final KafkaProducer<String ,String> kafkaProducer;
+    private final String topic;
+
+    private ProducerA(final KafkaProducer<String ,String> kafkaProducer,final String topic) {
+        this.kafkaProducer = kafkaProducer;
+        this.topic = topic;
+    }
+
+    public static ProducerA createAndStart(final KafkaConfig kafkaConfig) {
+        final ProducerA producer = create(kafkaConfig);
+        producer.start();
+        return producer;
+    }
+
+    public static ProducerA create(final KafkaConfig kafkaConfig) {
+        return new ProducerA(createKafkaProducer(kafkaConfig), kafkaConfig.sourceTopic());
+    }
+
+    private static KafkaProducer<String ,String> createKafkaProducer(final KafkaConfig kafkaConfig) {
+        final Properties producerConfig = new Properties();
+        producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.bootstrapSever());
+        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        final KafkaProducer kafkaProducer = new KafkaProducer<String, String>(producerConfig);
+        return kafkaProducer;
+    }
+
+    public void start() {
+        Observable
+                .interval(1, TimeUnit.SECONDS)
+                .doOnNext(n -> {
+                    final String key = n.toString();
+                    final String value = n.toString();
+                    System.out.println("before send k:" + key + " v:" + value);
+                    final Future<RecordMetadata> recordMetadataFeature =  kafkaProducer.send(new ProducerRecord(topic, key,value));
+                    recordMetadataFeature.get();
+                    System.out.println("send k:" + key + " v:" + value);
+                })
+                .subscribe();
+    }
 }
